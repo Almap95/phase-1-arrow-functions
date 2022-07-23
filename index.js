977ccf381f3890dc285aef9b0ec6cd6197a01e73
Alma Perez <aperez32@student.scf.edu>
2022-07-23 02:44:53 -0400

    Arrow Functions

---
+++ index.js
@@ -0,0 +1,6 @@
+//const divide = (2000,100)
+const divide = (parameter1, parameter2) => parameter1 / parameter2;
+divide(2000/100) ; 
+const square = x => x * x;
+const add = (parameter1, parameter2) => parameter1 + parameter2;
+add(2,3);
